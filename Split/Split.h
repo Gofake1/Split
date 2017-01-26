dc045cadae3aa347edd727c24205efadb7a6f0a3
David Wu <david@gofake1.net>
2017-01-25 21:58:45 -0800

    Initial Commit

---
+++ Split/Split.h
@@ -0,0 +1,19 @@
+//
+//  Split.h
+//  Split
+//
+//  Created by David Wu on 1/25/17.
+//  Copyright © 2017 Gofake1. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Split.
+FOUNDATION_EXPORT double SplitVersionNumber;
+
+//! Project version string for Split.
+FOUNDATION_EXPORT const unsigned char SplitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Split/PublicHeader.h>
+
+
